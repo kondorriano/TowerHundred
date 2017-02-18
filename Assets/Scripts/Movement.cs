0d0915451376c9d2f539441c0a377ce8dccdf0c4
Fonserbc <Fonserbc@gmail.com>
2017-02-18 11:50:34 +0100

    Derp missing files

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+
+    public Animator anim;
+	
+	// Update is called once per frame
+	void Update () {
+        anim.SetFloat("HorizontalAxis", Input.GetAxis("Horizontal"));
+        anim.SetFloat("VerticalAxis", Input.GetAxis("Vertical"));
+    }
+}
